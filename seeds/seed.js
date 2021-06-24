b1e1faf49883eb8b510fbe70483007b825abfc74
aanolan51 <a.a.nolan@hotmail.com>
2021-06-24 16:36:29 -0700

    Added comment seed to the seeds.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,6 +3,7 @@ const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,6 +14,8 @@ const seedDatabase = async () => {
   });
 
   const posts = await Post.bulkCreate(postData);
+
+  const comments = await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
