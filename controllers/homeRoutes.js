41c7a267f3b1ac3cf14e67cd5ffaf2253872dfd5
aanolan51 <a.a.nolan@hotmail.com>
2021-06-24 16:09:25 -0700

    Added in seeds and succesfully seeded database with user and post info. Homeroutes in progress.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,29 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
+// DO we need to bring in the connection file? 
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all blog posts and JOIN with the username who wrote the post:
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it:
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
