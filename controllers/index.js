4917ce9b3460ecbd21b4ffe5c0b2e3483902684f
aanolan51 <a.a.nolan@hotmail.com>
2021-06-24 15:26:50 -0700

    Added in homepage route to controller folder and index.js file.

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 
 //Bring in paths from the API routes:
 const apiRoutes = require('/api');
-//Bring in the dashboard path:
+//Bring in the dashboard:
 const dashboard = require('./dashboardRoutes');
+//Bring in the homepage:
+const homepage = require('./homeRoutes');
 
 //Use the the routes and set the path:
 router.use('/api', apiRoutes);
-router.use('/', dashboard);
+router.use('/dashboard', dashboard);
+router.use('/', homepage);
 
 module.exports = router;
