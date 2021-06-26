f6de43069339763fd5e15a3321f22fee755c7ead
aanolan51 <a.a.nolan@hotmail.com>
2021-06-25 18:20:13 -0700

    Added the login view handlebar

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-//Add in withAuth to include the authorization for logins. Only go to dashboard page when logged in:
+//Add in withAuth to include the authorization for logins. Only go to dashboard page when logged in. Show all posts for the user:
 router.get('/dashboard', withAuth, async (req, res) => {
     try {
       // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
+      const userPostData = await Post.findAll(req.session.user_id, {
         attributes: { exclude: ['password'] },
-        include: [{ model: Post }, {model: Comment}],
+        include: [{ model: User }, {model: Comment, include:{model:User}}],
       });
   
-      const user = userData.get({ plain: true });
+      const user = userPostData.get({ plain: true });
   
       res.render('dashboard', {
         ...user,
@@ -21,5 +21,10 @@ router.get('/dashboard', withAuth, async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  //Include an edit button for one post. When button clicked, bring to edit page. Find the one post by id:
+
+
+  //Include a new post create button/path:
 
   module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,7 +12,6 @@ router.get('/', async (req, res) => {
         include: [
           {
             model: User,
-            attributes: ['username'],
           },
         ],
       });
