0d7170f53a49ae2c42bc532388aad85747be0b91
aanolan51 <a.a.nolan@hotmail.com>
2021-07-05 14:19:08 -0700

    Added in if statement to connection.js file to connect to heroku database

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,28 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+let sequelize;
+
+//Set an if statement to either connect to a heroku database or a local database:
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  })
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
+
 
 module.exports = sequelize;
